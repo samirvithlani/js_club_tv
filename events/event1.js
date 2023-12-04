ac9cc922652715fe36972fa5b455b9007ba0a714
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-04 10:34:49 +0530

    on click

--- events/event1.js
+++ events/event1.js
@@ -1,5 +1,30 @@
 // var x = document.getElementById("text");
 // console.log("x-->",x);
 
-document.getElementById("text").innerHTML = "Hello World";
-document.getElementById("text").style.color = "red";
+function changeData() {
+  //   alert("Hello World");
+
+  document.getElementById("main").style.backgroundColor = "yellow";
+
+
+  document.getElementById("text").innerHTML = "Hello World";
+  document.getElementById("text").style.color = "red";
+
+  document.getElementById("google").href = "https://www.facebook.com";
+  document.getElementById("google").innerHTML = "Facebook";
+}
+
+function changeData1(){
+
+
+    //alert("Hello World");
+    var main1 = document.getElementById("main1")
+    main1.style.height = "200px";
+    main1.style.width = "200px";
+    main1.style.backgroundColor = "red";
+    // main1.style.borderRadius = "50%";
+    // main1.style.margin = "auto";
+    // main1.style.marginTop = "100px";
+    // main1.style.textAlign = "center";
+    
+}
